96aa43a4c08aaa66223a8713a1d8d33662e723b3
alsiddegmohmmed <alsiddeg.omer19990@gmail.com>
2023-11-18 18:22:07 +0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,31 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () =>{
+    const header = document.getElementById('header');
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 50) {
 
-
+        return header.classList.add('scroll-header');
+        
+    } else {
+        return header.classList.remove('scroll-header');
+    }
+}
+window.addEventListener('scroll', scrollHeader);
 /*=============== SWIPER POPULAR ===============*/
+var swiperPopular = new Swiper(".popular__container", {
+    spaceBetween: 32, 
+    grabCursor: true, 
+    centeredSlides: true, 
+    slidesPerview: 'auto',
+    loop:true, 
 
+    // im here start from 50.00 
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 
 /*=============== VALUE ACCORDION ===============*/
 
